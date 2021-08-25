3c1cf070f29e0da5b60ed439b4483952f85d09cf
aishwarya2700 <aishu2700@gmail.com>
2021-08-25 11:52:55 +0530

    initial commit

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace gitpractice
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 10, b = 20;
+            int c = a + b;
+            Console.WriteLine(c);
+        }
+    }
+}
